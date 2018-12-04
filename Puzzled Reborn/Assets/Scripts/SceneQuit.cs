a8cba2350ef2333672ec2fd4ae7259ec19df2216
bhitz <42984651+bhitz@users.noreply.github.com>
2018-12-03 21:47:22 -0700

    Uploading Scene Quit Script

---
+++ Puzzled Reborn/Assets/Scripts/SceneQuit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneQuit : MonoBehaviour {
+
+// Update is called once per frame
+	public void QuitScene(){
+		Application.Quit();		
+	
+	}
+
+}
