7a8355b92cf4942acc530acd1cf3f9957053a64c
Rafael Lopez <raflopjr@gmail.com>
2018-12-03 20:04:48 -0700

    Added basic functionality for active tetromino destination.

---
+++ Puzzled Reborn/Assets/Scripts/Tetromino.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour {
+
+	public Transform[] cubes;
+	public float reach = 100f;
+	public Color activeColor;
+	RaycastHit hit;
+
+	// Update is called once per frame
+	void Update () {
+		// Highlight projected drop location.
+		for(int i=0; i < 4; i++) {
+			// Get position of current cubes bottom face.
+			Vector3 pos = new Vector3(
+					cubes[i].position.x,
+					cubes[i].position.y - 0.5f,
+					cubes[i].position.z
+					);
+
+			if(Physics.Raycast(pos, Vector3.down, out hit, reach)) {
+				GameObject target = hit.collider.gameObject;
+				target.GetComponent<Renderer>().material.color = activeColor;
+			}
+		}
+	}
+
+	public void CleanUp() {
+		;
+	}
+}
