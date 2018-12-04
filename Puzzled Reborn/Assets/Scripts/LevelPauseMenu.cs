4ea28b642a20b0d1428ab2739dbcd67bc85f6541
bhitz <42984651+bhitz@users.noreply.github.com>
2018-12-03 21:46:19 -0700

    Uploading Level Pause Script

---
+++ Puzzled Reborn/Assets/Scripts/LevelPauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPauseMenu : MonoBehaviour {
+
+	public GameObject panel; 
+	//public SumPause status;
+
+	void Update(){
+		Debug.Log(SumPause.status);
+		if(SumPause.status == true){
+			panel.SetActive(true);
+			Time.timeScale = 0f;
+		} else{
+			panel.SetActive(false);
+			Time.timeScale = 1f;
+		}
+	}
+
+	// public void pauseGame(){
+	// 	panel.SetActive(true);
+	// 	Time.timeScale = 0f;
+
+	// }
+
+	// public void unpauseGame(){
+	// 	panel.SetActive(false);
+	// 	Time.timeScale = 1f;
+	// }
+}
