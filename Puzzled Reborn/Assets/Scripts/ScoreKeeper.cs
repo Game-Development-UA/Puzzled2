c898b984481cbc8d546afd4a98410338d239091e
bhitz <42984651+bhitz@users.noreply.github.com>
2018-12-08 15:33:52 -0700

    Adding in score keeper changes and changing ui color

---
+++ Puzzled Reborn/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreKeeper : MonoBehaviour {
+	[Range(0.1f, 10f)][SerializeField] float speed = 1f;
+	[SerializeField] int scorePerBlock = 50;
+	[SerializeField] TextMeshProUGUI scoreText;
+	[SerializeField] int score = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+		scoreText.text = score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Time.timeScale = speed;
+	}
+
+	public void AddToScore(){
+		score += scorePerBlock;
+		scoreText.text = score.ToString();
+	}
+}
